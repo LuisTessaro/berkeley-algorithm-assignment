f88e5652fe79a483f34831fd52977045f28e10ed
Allan Jorge <contato.allanj@gmail.com>
2018-11-09 13:13:28 -0200

    server: refactor some old code

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 const socketClient = require('socket.io-client')
 const socket = socketClient('http://127.0.0.1:8080')
 
-const time = parseInt(process.argv[2], 10)
+let time = parseInt(process.argv[2], 10)
 
 socket.on('getTime', function() {
   console.log('Sending time: ' + time)
